69d93b048f58e48885e119a12cc3e644bf9d7d65
Aaron Schmid <aaron@clab.rocks>
2024-03-18 19:07:33 +0100

    inital

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Python shell flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, mach-nix, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        mach = mach-nix.lib.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python311
+            python311Packages.pip
+            python311Packages.virtualenv
+            # Packages for jupiter
+            python311Packages.ipykernel
+            python311Packages.ipython
+            python311Packages.notebook
+            # Packages for project
+            python311Packages.numpy
+            python311Packages.pandas
+          ];
+
+          shellHook = ''
+            python -m venv ./.venv
+            . ./.venv/bin/activate
+          '';
+        };
+      }
+    );
+}
